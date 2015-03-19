6c8400a60dfb60f8c416c39f1cfd186feffb3135
Shebeda Olga <Olk6047@yandex.ru>
2015-03-19 14:29:06 +0300

    new version

--- MathСalculator.Domain/Methods/Method_3_8.cs
+++ MathСalculator.Domain/Methods/Method_3_8.cs
@@ -10,8 +10,9 @@ namespace MathСalculator.Domain.Methods
     {
         public class CMethodSimpsona
         {
+
             /// <summary>
-            /// метод парабол/симпсона решения интегрального уровнения
+            /// метод парабол/симпсона
             /// </summary>
             /// <param name="function">подинтегральная функция</param>
             /// <param name="a">начало отрезка [a, b]</param>
@@ -19,7 +20,42 @@ namespace MathСalculator.Domain.Methods
             /// <returns></returns>
             public double MethodSimpsona(string function, double a, double b)
             {
-                return (a - b) / 6 * (CalcMethods.ParseFunction(function, a) + 4 * CalcMethods.ParseFunction(function, (a + b) / 2) + CalcMethods.ParseFunction(function, b));
+                double h = (a - b)/1000;
+                double x0 = a;
+                double x1 = x0 + h;
+                double s = 0;
+                for (int i = 0; i <= n - 1; i++)
+                {
+                    S += CalcMethods.ParseFunction(function, x0) + 4 * CalcMethods.ParseFunction(function, (x0 + h / 2)) + CalcMethods.ParseFunction(function, x1);
+                    x0 = x1;
+                    x1 += h;
+                }
+
+                return s;
+            }
+
+            /// <summary>
+            /// метод 3/8
+            /// </summary>
+            /// <param name="function">подинтегральная функция</param>
+            /// <param name="a">начало отрезка [a, b]</param>
+            /// <param name="b">конец отрезка [a, b]</param>
+            /// <returns></returns>
+            public double Method_3_8(string function, double a, double b)
+            {
+                double h = (b - a) / (3 * n);
+                S = CalcMethods.ParseFunction(function, a) + CalcMethods.ParseFunction(function, b);
+                int m = 3 * n - 1;
+                double x;
+                for (int i = 1; i <= m; i++)
+                {
+                    x = a + h * i;
+                    if (i % 3 == 0)
+                        S = S + 2 * CalcMethods.ParseFunction(function, x);
+                    else
+                        S = S + 3 * CalcMethods.ParseFunction(function, x);
+                }
+                S = S * 3 / 8 * h;
             }
         }
     }
