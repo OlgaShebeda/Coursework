6ffab9447ea4e08a347d8795205d9b7fe6df0044
Shebeda Olga <Olk6047@yandex.ru>
2015-03-31 20:42:00 +0300

    QuasiNewtons

--- MathСalculator.Domain/Methods/IterationMethod.cs
+++ MathСalculator.Domain/Methods/IterationMethod.cs
@@ -130,5 +130,35 @@ namespace MathСalculator.Domain.Methods
             resultTEXT += "Результат =" + y + "\n";
             return y;
         }
+
+        /// <summary>
+        /// Квазиньютоновский итерационный метод
+        /// </summary>
+        /// <param name="Function"></param>
+        /// <param name="X0">начальное приближение</param>
+        /// <param name="Epsilon"></param>
+        /// <returns></returns>
+        public double Quasi_NewtonMethod(string Function, double X0, double Epsilon)
+        {
+            double bet = 0.01;
+            double betLast = 0;
+            double gam = 0.0001;
+            double y = X0 - bet * CalcMethods.ParseFunction(Function, X0) / CalcMethods.DerivativeOfFunction(Function, X0);
+            double k = 1;
+            string resultTEXT = "Функция x = f(x) = " + Function + "\n";
+            while ((Math.Abs(y - X0) > Epsilon))
+            {
+                if (k > 500) throw new ArgumentException("k > 500");
+                resultTEXT += "\n y = " + y;
+                k++;
+                resultTEXT += "\n k =" + k + "\n";
+                bet = Math.Abs(CalcMethods.ParseFunction(Function, X0)) * gam / Math.Abs(CalcMethods.ParseFunction(Function, y)) * bet;
+                gam = gam * Math.Abs(CalcMethods.ParseFunction(Function, X0)) * bet / Math.Abs(CalcMethods.ParseFunction(Function, y)) * betLast;
+                X0 = y;
+                y = X0 - CalcMethods.ParseFunction(Function, X0) / CalcMethods.DerivativeOfFunction(Function, X0);
+            }
+            resultTEXT += "Результат =" + X0 + "\n";
+            return y;
+        }
     }
 }
